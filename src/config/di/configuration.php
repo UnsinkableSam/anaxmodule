48280eb3e6eafad76d809336d0bb6d5b3c36126b
Unsinkablesam <maraphostv@gmail.com>
2018-12-10 08:32:06 +0100

    added new json stuff

--- src/config/di/configuration.php
+++ src/config/di/configuration.php
@@ -10,7 +10,7 @@ return [
             "shared" => true,
             "callback" => function () {
                 $config = new \Anax\Configure\Configuration();
-                $dirs = require __DIR__ . "/../configuration.php";
+                $dirs = require ANAX_INSTALL_PATH . "/config/configuration.php";
                 $config->setBaseDirectories($dirs);
                 return $config;
             }
